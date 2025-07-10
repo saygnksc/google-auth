2c9cb6910d7d502d10dcf0086e5ce042fd2c5e56
saygnksc <saygnksc@gmail.com>
2025-07-11 02:53:16 +0300

    Create google-login.js

---
+++ api/google-login.js
@@ -0,0 +1,23 @@
+import { google } from 'googleapis';
+
+export default async function handler(req, res) {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/google-callback`
+  );
+
+  const scopes = [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'
+  ];
+
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes,
+    prompt: 'consent',
+    redirect_uri: `${process.env.NEXT_PUBLIC_BASE_URL}/api/google-callback`
+  });
+
+  res.status(200).json({ url });
+}
